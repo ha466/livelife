15b5e90dc6a4219821adc8c78b31efcaed9aee4e
Evking <58391348+ha466@users.noreply.github.com>
2026-05-20 19:51:22 +0530

    Add main.jsx to render the App component

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from '../OneFile.jsx'; // Imports the core laboratory web application
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
